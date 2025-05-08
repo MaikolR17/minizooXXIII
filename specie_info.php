4b6d14d93b31047515475300323866ebdaf0a6c6
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-05-08 10:22:37 -0300

    Minimo ajuste de archivos y rutas

--- specie_info.php
+++ specie_info.php
@@ -20,6 +20,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=htmlspecialchars($specie[0]['name'])?></title>
+    <link rel="stylesheet" href="CSS/specie_info.css">
 </head>
 <body>
     <main>
